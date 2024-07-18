ccb80866c47825be0925f420c1fe3b472e8e66f7
Soh Hong Yu <sohhongyu@gmail.com>
2024-07-19 01:43:33 +0800

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -15,7 +15,7 @@ const greeting = () => async (ctx: Context) => {
   const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
 
   if (messageId) {
-    await replyToMessage(ctx, messageId, `Greetings2, ${userName}!`);
+    await replyToMessage(ctx, messageId, `Test Greetings, ${userName}!`);
   }
 };
 
